e8ac013889a3beb27c63ccbf6903670fe9375427
Yifeng <ymao@splunk.com>
2015-02-06 14:48:37 +0800

    now test report shows aborted jobs and job statistics
    meanwhile, remove files from target/*

--- buildflow-results-aggregator/src/main/java/org/zeroturnaround/jenkins/flowbuildtestaggregator/FlowTestAggregator.java
+++ buildflow-results-aggregator/src/main/java/org/zeroturnaround/jenkins/flowbuildtestaggregator/FlowTestAggregator.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.zeroturnaround.jenkins.flowbuildtestaggregator.FlowTestResults.AbortedJob;
 
 import com.cloudbees.plugins.flow.BuildFlow;
 import com.cloudbees.plugins.flow.FlowRun;
@@ -27,62 +28,78 @@ import hudson.tasks.test.TestResultProjectAction;
 
 public class FlowTestAggregator extends Recorder {
 
-  @DataBoundConstructor
-  public FlowTestAggregator() {}
+	@DataBoundConstructor
+	public FlowTestAggregator() {
+	}
 
-  public BuildStepMonitor getRequiredMonitorService() {
-    return BuildStepMonitor.NONE;
-  }
-  
-  @Override
-  public Collection<? extends Action> getProjectActions(AbstractProject<?, ?> project) {
-	  //show test trend graph on job page
-      Action a = new TestResultProjectAction(project);
-      return Collections.singletonList(a);
-  }
+	public BuildStepMonitor getRequiredMonitorService() {
+		return BuildStepMonitor.NONE;
+	}
 
-  @Override
-  public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
-    FlowTestResults testResults = new FlowTestResults(build);
-    FlowRun flowRun = (FlowRun) build;
-    flowRun.addAction(testResults);
-    
-    listener.getLogger().println("Starting to gather test results!");
-    try {
-      for (JobInvocation jobInvocation : flowRun.getJobsGraph().vertexSet()) {
-        //if (!jobInvocation.getClass().getName().contains("Start")) {
-          TestResultAction testResult = jobInvocation.getBuild().getAction(hudson.tasks.junit.TestResultAction.class);
-          if (testResult != null) testResults.add(testResult);
-          AggregatedTestResultAction testResult1 = jobInvocation.getBuild().getAction(FlowTestResults.class);
-          if (testResult1 != null) testResults.add_all(testResult1);
-        //}
-      }
-    } catch (ExecutionException e) {
-      listener.getLogger().print("ERROR: " + e.getMessage());
-    }
-    return true;
-  }
+	@Override
+	public Collection<? extends Action> getProjectActions(
+			AbstractProject<?, ?> project) {
+		// show test trend graph on job page
+		Action a = new TestResultProjectAction(project);
+		return Collections.singletonList(a);
+	}
 
-  @Extension
-  public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+	@Override
+	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher,
+			BuildListener listener) throws InterruptedException, IOException {
+		FlowTestResults testResults = new FlowTestResults(build);
+		FlowRun flowRun = (FlowRun) build;
+		flowRun.addAction(testResults);
 
-  @Override
-  public DescriptorImpl getDescriptor() {
-    return DESCRIPTOR;
-  }
+		listener.getLogger().println("Starting to gather test results!");
+		try {
+			for (JobInvocation jobInvocation : flowRun.getJobsGraph()
+					.vertexSet()) {
+				// if (!jobInvocation.getClass().getName().contains("Start")) {
+				AggregatedTestResultAction testResult1 = jobInvocation
+						.getBuild().getAction(FlowTestResults.class);
+				if (testResult1 != null) {
+					testResults.add_all(testResult1);
+					continue;
+				}
+				TestResultAction testResult = jobInvocation.getBuild()
+						.getAction(hudson.tasks.junit.TestResultAction.class);
+				if (testResult != null) {
+					testResults.add(testResult);
+					continue;
+				}
+				// if you can reach here, then you are an aborted build
+				testResults.abortedjobs.add(new AbortedJob(jobInvocation
+						.getName(), jobInvocation.getBuild().getUrl()));
+				// }
+			}
+		} catch (ExecutionException e) {
+			listener.getLogger().print("ERROR: " + e.getMessage());
+		}
+		return true;
+	}
 
-  public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+	@Extension
+	public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
 
-    /**
-     * This human readable name is used in the configuration screen.
-     */
-    public String getDisplayName() {
-      return "Aggregate build flow test results";
-    }
+	@Override
+	public DescriptorImpl getDescriptor() {
+		return DESCRIPTOR;
+	}
 
-    @Override
-    public boolean isApplicable(Class jobType) {
-      return BuildFlow.class.equals(jobType);
-    }
-  }
+	public static final class DescriptorImpl extends
+			BuildStepDescriptor<Publisher> {
+
+		/**
+		 * This human readable name is used in the configuration screen.
+		 */
+		public String getDisplayName() {
+			return "Aggregate build flow test results";
+		}
+
+		@Override
+		public boolean isApplicable(Class jobType) {
+			return BuildFlow.class.equals(jobType);
+		}
+	}
 }
--- buildflow-results-aggregator/src/main/java/org/zeroturnaround/jenkins/flowbuildtestaggregator/FlowTestResults.java
+++ buildflow-results-aggregator/src/main/java/org/zeroturnaround/jenkins/flowbuildtestaggregator/FlowTestResults.java
@@ -1,13 +1,55 @@
 package org.zeroturnaround.jenkins.flowbuildtestaggregator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jenkins.model.Jenkins;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.tasks.test.AbstractTestResultAction;
 import hudson.tasks.test.AggregatedTestResultAction;
-import hudson.tasks.test.AggregatedTestResultAction.Child;
 
 public class FlowTestResults extends AggregatedTestResultAction {
+	
+	public static class AbortedJob {
+	    String name;
+		String url;
+
+		public AbortedJob(String name, String url) {
+			this.name = name;
+			this.url = url;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getUrl() {
+			return url;
+		}
+
+	}
+	
+	int failedjob = 0;
+	int successjob = 0;
+
+	List<AbortedJob> abortedjobs = new ArrayList<AbortedJob>();
+
+	public List<AbortedJob> getAbortedjobs() {
+		return abortedjobs;
+	}
+
+	public int getFailedJobsNumber() {
+		return failedjob;
+	}
+
+	public int getSuccessJobsNumber() {
+		return successjob;
+	}
+
+	public int getAbortedJobsNumber() {
+		return abortedjobs.size();
+	}
 
 	public FlowTestResults(AbstractBuild owner) {
 		super(owner);
@@ -27,13 +69,33 @@ public class FlowTestResults extends AggregatedTestResultAction {
 
 	@Override
 	protected void add(AbstractTestResultAction child) {
+		// collect test result statictics information on JOB level
+		if (child.getFailCount() > 0) {
+			failedjob++;
+		} else {
+			successjob++;
+		}
+
+		// collect test result statictics information on CASE level
 		super.add(child);
 	}
 
 	protected void add_all(AggregatedTestResultAction child) {
+		// collect test result statictics information on JOB level
+
+		failedjob += ((FlowTestResults) child).getFailedJobsNumber();
+		successjob += ((FlowTestResults) child).getSuccessJobsNumber();
+		abortedjobs.addAll(((FlowTestResults) child).abortedjobs);
+
+		// collect test result statictics information on CASE level
+
+		// a workaround for composite reports
+		// the reason why add child and then remove it is that cases status can
+		// be counted
 		super.add(child);
-		
+
 		children.remove(children.size() - 1);
 		children.addAll(child.children);
 	}
 }
+
